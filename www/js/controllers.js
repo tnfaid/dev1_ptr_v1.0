e466218f4c8da1d3976d45066263cdf4ee775f22
TinjaBoegil <bram692.didu@gmail.com>
2018-04-23 11:59:23 +0700

    adding accordion effect

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,17 +18,35 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('judulBeritaCtrl', ['$scope', '$stateParams',
+.controller('judulBeritaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
 
 }])
    
-.controller('pemasaranCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('pemasaranCtrl', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
 
 }])
    
@@ -80,9 +98,7 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('fAQsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('fAQsCtrl', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
 
 
@@ -623,4 +639,11 @@ function ($scope, $stateParams) {
 
 
 }])
- 
+   
+.controller('wartaKBLCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
